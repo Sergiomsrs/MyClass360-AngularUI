fa967602fcc4dd485e46da8631fa17896fadb88d
Sergio <sergiomsrs@gmail.com>
2025-08-23 11:22:01 +0200

    chore: añadiendo tailwind, daisyui y estructura inicial

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { LandingPageComponent } from './pages/landing-page/landing-page.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: LandingPageComponent,
+    },
+    {
+        path: "landing",
+        component: HomePageComponent,
+    },
+];
